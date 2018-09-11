a6b6fca12a6f733815cd9611aba42633365d44f2
Jun Ji <junji641220@gmail.com>
2018-09-11 12:56:52 +0900

    Revert "Revert "version3 text added""
    This reverts commit eaaf1607aa4fba071361d8fa202a1071b97fd1ae.

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ Public class Main {
 	public static void main(String[] args) {
 	System.out.println("Hello Web&App!");
 	System.out.println("Vertion-2 test");
+	System.out.println("Vertion-3 test");
+
 	}
 }
