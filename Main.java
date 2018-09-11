556586910b6a0144648446faecb46d70921c3f61
Jun Ji <junji641220@gmail.com>
2018-09-11 12:53:50 +0900

    Revert "Revert "version-2""
    This reverts commit 422cb23874657e009cc326203454d21d3adc4564.

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
 Public class Main {
 	public static void main(String[] args) {
 	System.out.println("Hello Web&App!");
-<<<<<<< HEAD
 	System.out.println("Vertion-2 test");
 	System.out.println("Vertion-3 test");
 
-=======
->>>>>>> parent of 8651d4c... version-2
 	}
 }
